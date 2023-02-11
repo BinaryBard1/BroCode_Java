0f6f9aba1b42a2f1a44e01bbf7ab1b3158710e0d
BinaryBard <shrikhandeaniruddha@gmail.com>
2023-02-11 17:44:32 +0530

    ASD

---
+++ dsaq/extraHashmap.java
@@ -0,0 +1,22 @@
+package dsaq;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class extraHashmap {
+    public static void main(String[] args) {
+        HashMap<String, String> namep = new HashMap<>();
+        ArrayList <Integer> result = new ArrayList<>();
+        namep.put("ani","123456");
+        namep.put("trw","123456");
+        namep.put("trtw","1234567");
+
+        String result1 = namep.get("ani");
+        System.out.println(result);
+        int [] num = {1,2,3,4};
+        int key = 3;
+        int sum = 0;
+        int aaa;
+
+
+    }
+}
--- dsaq/part202DArraylist.java
+++ dsaq/part202DArraylist.java
@@ -1,4 +1,9 @@
 package dsaq;
 
+import java.util.ArrayList;
+
 public class part202DArraylist {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> vit = new ArrayList<>();
+    }
 }
