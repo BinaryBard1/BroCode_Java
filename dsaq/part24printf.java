590db5aa5d5bee77e264e24881bc7395e8c053f7
BinaryBard <shrikhandeaniruddha@gmail.com>
2023-02-11 19:45:08 +0530

    ASD

---
+++ dsaq/part24printf.java
@@ -0,0 +1,12 @@
+package dsaq;
+
+public class part24printf {
+    public static void main(String[] args) {
+        System.out.printf("Hi %-30c I have just started coding ",'!');//for width after the char
+        System.out.println();
+        System.out.printf("I am %1d years old", 22);//for width before the char
+        System.out.println();
+        System.out.printf("My percentile is %.2f", 88.98);
+
+    }
+}
---
+++ dsaq/part25final.java
@@ -0,0 +1,9 @@
+package dsaq;
+
+public class part25final {
+    public static void main(String[] args) {
+        final double pi = 3.13243;
+        pi = 4;
+        System.out.println(pi);
+    }
+}
